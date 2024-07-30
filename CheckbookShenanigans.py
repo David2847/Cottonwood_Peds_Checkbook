6ebf1e9d565a9dee8641bcc783d1f8e1c868805b
David2847 <jantzd@oregonstate.edu>
2024-07-30 10:03:31 -0500

    Income half is theoretically finished. Doc strings for methods and further testing attempts to break it still needed.

--- CheckbookShenanigans.py
+++ CheckbookShenanigans.py
@@ -21,18 +21,32 @@ class Income:
         for row in self._data_list:
             print(row)
 
-    def revise_column_titles(self):
+    def rename_columns(self):
         """ Renames column titles to their new names. Ignores columns to be deleted."""
         # Mapping of old keys to new keys
-        # todo: this key_mapping is unfinished! Also maybe it really only needs to handle Amount.
         key_mapping = {
             'Amount': 'Total Amount',
-            'Category': 'Copays'
         }
 
+        # go through every key value pair in every row and rename if needed... not very efficient :(
         for row_num in range(len(self._data_list)):
+            # Uses dictionary comprehension
             new_row = {key_mapping.get(k, k): v for k, v in self._data_list[row_num].items()}
             self._data_list[row_num] = new_row
+
+    def remove_unnecessary_columns(self):
+        for row in self._data_list:
+            del row['Check No']
+            del row['Category']
+            for i in range(1, 7):
+                try: # sometimes there are fewer than 6 apparently
+                    del row['Split Category ' + str(i)]
+                    del row['Split Amount ' + str(i)]
+                    del row['Split Memo ' + str(i)]
+                    del row['Split Class ' + str(i)]
+                except KeyError:
+                    break
+
 
     def write_to_csv(self):
         column_titles = self.get_column_titles()
@@ -83,11 +97,42 @@ class Income:
                 row['Transfers Received'] = row['Amount']
             # if category column is empty: that means the amount is split. iterate through row until finding a D.
             # then move the value to the right of it into Copays. keep iterating through until finding an E. then
-            # move the value to the right of it into Third Party Payments. Same for F and G. that should be it.
+            # move the value to the right of it into Third Party Payments. Same for F and G. This else statement
+            # also handles a weird edge case where there is no letter anywhere in the row. This means it is actually
+            # NOT a split payment and we need to look at the Payee information to decide if it should go in Copays
+            # or Third Party Payments.
             else:
-                for value in row.values():
-                    ok, so this is where I need to index the dictionary as I search it so that I can grab the value to the right
+                is_blank_field_edge_case = True
+                indexed_keys = list(row.keys())
+                # Iterate with index
+                for i in range(len(indexed_keys)):
+                    key = indexed_keys[i]
+                    if row[key] == 'D':
+                        next_key = indexed_keys[i + 1]
+                        split_amount = row[next_key]
+                        row['Copays'] = split_amount
+                        is_blank_field_edge_case = False
+                    elif row[key] == 'E':
+                        next_key = indexed_keys[i + 1]
+                        split_amount = row[next_key]
+                        row['Third Party Payments'] = split_amount
+                        is_blank_field_edge_case = False
+                    elif row[key] == 'F':
+                        next_key = indexed_keys[i + 1]
+                        split_amount = row[next_key]
+                        row['Refunds Received'] = split_amount
+                        is_blank_field_edge_case = False
+                    elif row[key] == 'G':
+                        next_key = indexed_keys[i + 1]
+                        split_amount = row[next_key]
+                        row['Transfers Received'] = split_amount
+                        is_blank_field_edge_case = False
 
+                if is_blank_field_edge_case:
+                    if 'Terminal' in row['Payee']:
+                        row['Copays'] = row['Amount']
+                    else:
+                        row['Third Party Payments'] = row['Amount']
 
 
 class Expenses:
@@ -95,8 +140,7 @@ class Expenses:
 
 
 class InitialCheckbook:
-    """ Parent class for everything... Reads into python data object, does initial data manipulation,
-    then splits data into income and expenses objects. """
+    """ Reads file into python data object, does initial data manipulation """
 
     def __init__(self, input_file):
         """
@@ -104,7 +148,7 @@ class InitialCheckbook:
             and the keys are the column headers.
         """
 
-        with open('april_input.csv', mode='r', newline='', encoding='utf-8-sig') as input_file:
+        with open(input_file, mode='r', newline='', encoding='utf-8-sig') as input_file:
             csv_reader = csv.DictReader(input_file)
             self._data_list = []
 
@@ -139,29 +183,34 @@ class InitialCheckbook:
         # self.display()
 
 
-
-
 def main():
+    input_file = input("What file would you like to process?\nMake sure it is located in the same folder as this "
+                       "script.\nSpell it perfectly with correct capitalization and "
+                       "include .csv at the end\n")
     # open up csv file, store in a list of dicts
-    transactions = InitialCheckbook('april_input.csv')
+    transactions = InitialCheckbook(input_file)
     # reformat dates into integers, this is required for both income and expenses
     transactions.dates_to_day_numbers()
     # split data into income and expenses
     income = Income(transactions)
-    # take care of columns D and E based on adjacent letter. makes new columns at the end.
-    #       Theoretically also handles F and G.
+    # take care of columns D, E, F, and G based on adjacent letter. makes new columns at the end.
     income.categorize_payments()
     # rename columns
+    income.rename_columns()
     # remove extra columns
+    income.remove_unnecessary_columns()
     # Write to csv
     income.write_to_csv()
 
-
     """
-    income:
+    income remaining tasks:
         take care of yellow highlighted edge cases in excel file
         maybe do a check to make sure total amount is actually the sum of the others
-        output to new csv file
+        go back through and add docstrings and comments
+        try to break it in a bunch of different ways
+            wrong file name
+            ... brainstorm
+        
     expenses:
         reorder / rename / add headers
         make amounts positive
